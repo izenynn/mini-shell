ecfb39c6877038bc123e569fab30689a6d5bd689
izenynn <me@izenynn.com>
2021-11-20 13:57:20 +0100

    Add history but not working when there are ws

--- src/prompt/prompt.c
+++ src/prompt/prompt.c
@@ -6,36 +6,50 @@
 /*   By: dpoveda- <me@izenynn.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 09:32:44 by dpoveda-          #+#    #+#             */
-/*   Updated: 2021/11/15 20:01:28 by dpoveda-         ###   ########.fr       */
+/*   Updated: 2021/11/20 13:53:51 by dpoveda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <readline/readline.h>
 #include <sh.h>
 
-// TODO function that gets current working directory from path
+/* get line */
+static char	*rl_gets(void)
+{
+	char	*line;
+
+	line = readline("❯ " FG_DEF);
+	if (line && *line)
+		add_history(line);
+	return (line);
+}
+
+/* prompt message */
+static void	print_prompt_msg(void)
+{
+	char	*dir;
+	char	*prompt_clr;
+
+	dir = (char *)malloc(PATH_MAX * sizeof(char));
+	if (getcwd(dir, PATH_MAX) == NULL)
+		dir = NULL;
+	dir[PATH_MAX - 1] = '\0';
+	if (g_sh.status == EXIT_SUCCESS)
+		prompt_clr = ft_strdup(FG_MAG);
+	else
+		prompt_clr = ft_strdup(FG_RED);
+	ft_dprintf(STDERR_FILENO, "\n" FG_BLU "%s" FG_DEF "\n" "%s",
+		dir, prompt_clr);
+	free(dir);
+	free(prompt_clr);
+}
 
 /* read input on prompt */
 char	*prompt_read_input(void)
 {
 	char	*line;
-	char	*dir;
-	char	*prompt_clr;
 
-	// get cwd
-	dir = (char *)malloc(PATH_MAX * sizeof(char));
-	if (getcwd(dir, PATH_MAX) == NULL)
-		dir = NULL;
-	dir[PATH_MAX - 1] = '\0';
-
-	// get color
-	if (g_sh.status == EXIT_SUCCESS)
-		prompt_clr = FG_MAG;
-	else
-		prompt_clr = FG_RED;
-
-	// print prompt
-	printf("\n" FG_BLU "%s" FG_DEF "\n" "%s", dir, prompt_clr);
-	line = readline("❯ " FG_DEF);
-	free(dir);
+	print_prompt_msg();
+	line = rl_gets();
 	return (line);
 }
--- src/prompt/prompt_init.c
+++ src/prompt/prompt_init.c
@@ -6,7 +6,7 @@
 /*   By: dpoveda- <me@izenynn.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 12:17:04 by dpoveda-          #+#    #+#             */
-/*   Updated: 2021/11/12 11:49:05 by dpoveda-         ###   ########.fr       */
+/*   Updated: 2021/11/20 13:27:00 by dpoveda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,7 @@
 /* welcome message on start */
 static void	welcome(void)
 {
-	/*printf("\n"\
-	FG_YEL "           _       _ " FG_RED "    _          _ _ \n"\
-	FG_YEL " _ __ ___ (_)_ __ (_)" FG_RED "___| |__   ___| | |\n"\
-	FG_YEL "| '_ ` _ \\| | '_ \\| " FG_RED "/ __| '_ \\ / _ \\ | |\n"\
-	FG_YEL "| | | | | | | | | | " FG_RED "\\__ \\ | | |  __/ | |\n"\
-	FG_YEL "|_| |_| |_|_|_| |_|_" FG_RED "|___/_| |_|\\___|_|_|\n"\
-	FG_DEF);*/
-
-	/*printf("\n"\
-	FG_GRN "           oo          oo          dP                dP dP\n"\
-	FG_YEL "                                   88                88 88\n"\
-	FG_RED "88d8b.d8b. dP 88d888b. dP .d8888b. 88d888b. .d8888b. 88 88\n"\
-	FG_MAG "88'`88'`88 88 88'  `88 88 Y8ooooo. 88'  `88 88ooood8 88 88\n"\
-	FG_BLU "88  88  88 88 88    88 88       88 88    88 88.  ... 88 88\n"\
-	FG_CYN "dP  dP  dP dP dP    dP dP `88888P' dP    dP `88888P' dP dP\n"\
-	FG_DEF);*/
-
-	printf("\n"\
+	ft_dprintf(STDERR_FILENO, "\n"\
 	FG_YEL "           oo          oo " FG_GRN "         dP                dP dP\n"\
 	FG_YEL "                          " FG_YEL "         88                88 88\n"\
 	FG_YEL "88d8b.d8b. dP 88d888b. dP " FG_RED ".d8888b. 88d888b. .d8888b. 88 88\n"\
