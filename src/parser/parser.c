e8264e841926d360569cc2700ba0e849ab04580a
izenynn <me@izenynn.com>
2021-11-21 17:11:47 +0100

    Change grammar info to .h file

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,65 +6,11 @@
 /*   By: dpoveda- <me@izenynn.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 12:01:22 by dpoveda-          #+#    #+#             */
-/*   Updated: 2021/11/20 13:19:20 by dpoveda-         ###   ########.fr       */
+/*   Updated: 2021/11/21 17:03:47 by dpoveda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sh.h>
-
-/*
- * Grammar
- *
- * <command line>		:	<job> ';' <command line>
- * 						|	<job> ';'
- * 						|	<job> '&' <command line>
- * 						|	<job> '&'
- * 						|	<job>
- * 						;
- *
- * <job>				:	<command> '|' <job>
- * 						|	<command>
- * 						;
- *
- * <command>			:	<simple command> '<' <file>
- * 						|	<simple command> '>' <file>
- * 						|	<simple command>
- * 						;
- *
- * <simple command>		:	<name> <token list>
- * 						;
- *
- * <token list>			:	<token> <token list>
- * 						;
- *
- */
-
-/*
- * Create an ast recursively:
- *
- * cmd_line()		=>	test all command line in order
- * cmd_line_1()		=>	<job> ';' <command line>
- * cmd_line_2()		=>	<job> ';'
- * cmd_line_3()		=>	<job> '&' <command line>
- * cmd_line_4()		=>	<job> '&'
- * cmd_line_5()		=>	<job>
- *
- * job()			=>	test all job in order
- * job_1()			=>	<command> '|' <job>
- * job_2()			=>	<command>
- *
- * cmd()			=>	test all command in order
- * cmd_1()			=>	<simple command> '<' <file>
- * cmd_2()			=>	<simple command> '>' <file>
- * cmd_3()			=>	<simple command>
- *
- * simple_cmd()		=>	test simple cmd
- * simple_cmd_1()	=>	<name> <token list>
- *
- * tok_lst()		=>	test token list
- * tok_lst_1()		=>	<token> <token list>
- *
- */
 
 /* parse main function */
 int	parse(t_lexer *lex, t_ast **ast)
