d55c4386be1f03479e03afe99d588de0085a332b
acostal- <acostalcardenas@gmail.com>
2021-11-26 16:46:40 +0100

    H0t fix on getenv

--- src/utils/ft_getenv.c
+++ src/utils/ft_getenv.c
@@ -15,14 +15,16 @@
 char	*ft_getenv(const char *path)
 {
 	int		path_size;
+	char	*aux;
 	char	*ret;
 	t_list	*head;
 
 	head = g_sh.env;
-	path_size = ft_strlen(path);
+	aux = ft_strjoin(path, "=");
+	path_size = ft_strlen(aux);
 	while (g_sh.env)
 	{
-		if (ft_strncmp(path, (char *)g_sh.env->data, path_size) == 0)
+		if (ft_strncmp(aux, (char *)g_sh.env->data, path_size) == 0)
 		{
 			ret = ft_substr((char *)g_sh.env->data, path_size + 1,
 					ft_strlen((char *)g_sh.env->data) - path_size);
