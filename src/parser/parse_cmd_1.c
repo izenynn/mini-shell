7b4a74992c84a0a915199012d700e5de98ce6ee6
izenynn <me@izenynn.com>
2021-11-29 15:48:53 +0100

    move func to other file

--- src/parser/parse_cmd_1.c
+++ src/parser/parse_cmd_1.c
@@ -6,7 +6,7 @@
 /*   By: dpoveda- <me@izenynn.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 15:30:53 by dpoveda-          #+#    #+#             */
-/*   Updated: 2021/11/29 15:44:40 by dpoveda-         ###   ########.fr       */
+/*   Updated: 2021/11/29 15:48:35 by dpoveda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,4 +46,82 @@ t_ast	*cmd(void)
 		return (new_node);
 
 	return (NULL);
+}
+
+/* <redir in> <simple cmd> <redir out> */
+t_ast	*cmd_1(void)
+{
+	t_ast	*redir_in_nd;
+	t_ast	*redir_out_nd;
+	t_ast	*simple_cmd_nd;
+	t_ast	*redir_nd;
+	t_ast	*res;
+
+	// <redir in>
+	redir_in_nd = redir_in();
+	if (redir_in_nd == NULL)
+		return (NULL);
+	// <simple cmd>
+	simple_cmd_nd = simple_cmd();
+	if (simple_cmd_nd == NULL)
+	{
+		ast_del(redir_in_nd);
+		return (NULL);
+	}
+	// <redir out>
+	redir_out_nd = redir_out();
+	if (redir_out_nd == NULL)
+	{
+		ast_del(redir_in_nd);
+		ast_del(simple_cmd_nd);
+		return (NULL);
+	}
+	// create redir node
+	redir_nd = (t_ast *)malloc(sizeof(t_ast));
+	ast_settype(redir_nd, AST_REDIR);
+	ast_attach(redir_nd, redir_in_nd, redir_out_nd);
+	// create node
+	res = (t_ast *)malloc(sizeof(t_ast));
+	ast_settype(res, AST_CMD);
+	ast_attach(res, redir_nd, simple_cmd_nd);
+	return (res);
+}
+
+/* <redir out> <simple cmd> <redir in> */
+t_ast	*cmd_2(void)
+{
+	t_ast	*redir_in_nd;
+	t_ast	*redir_out_nd;
+	t_ast	*simple_cmd_nd;
+	t_ast	*redir_nd;
+	t_ast	*res;
+
+	// <redir out>
+	redir_out_nd = redir_out();
+	if (redir_out_nd == NULL)
+		return (NULL);
+	// <simple cmd>
+	simple_cmd_nd = simple_cmd();
+	if (simple_cmd_nd == NULL)
+	{
+		ast_del(redir_out_nd);
+		return (NULL);
+	}
+	// <redir in>
+	redir_in_nd = redir_in();
+	if (redir_in_nd == NULL)
+	{
+		ast_del(redir_out_nd);
+		ast_del(simple_cmd_nd);
+		return (NULL);
+	}
+	// create redir node
+	redir_nd = (t_ast *)malloc(sizeof(t_ast));
+	ast_settype(redir_nd, AST_REDIR);
+	ast_attach(redir_nd, redir_in_nd, redir_out_nd);
+	// create node
+	res = (t_ast *)malloc(sizeof(t_ast));
+	ast_settype(res, AST_CMD);
+	ast_attach(res, redir_nd, simple_cmd_nd);
+	return (res);
 }
