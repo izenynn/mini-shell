8fa63f1856a48dfcb632506e79c4d361abdc8a10
acostal- <acostal-@student.42madrid.com>
2021-12-10 17:08:37 +0100

    Fix norme on unset

--- src/built-in/unset.c
+++ src/built-in/unset.c
@@ -14,83 +14,88 @@
 
 #include <sh.h>
 
+static void	copy_and_concat(const char *unset, char *tmp)
+{
+	ft_strlcpy(tmp, unset, ft_strlen(tmp) + ft_strlen(unset));
+	tmp[ft_strlen(unset)] = '=';
+	tmp[ft_strlen(unset) + 1] = '\0';
+}
+
 /* move list to desired positon */
 static int	find_pos(const char *unset)
 {
-    int		cnt;	
-    char	*tmp;
+	int		cnt;
+	char	*tmp;
 
-    if (unset == NULL)
-        return (1);
-    tmp = (char *)malloc(sizeof(char) * (ft_strlen(unset) + 2));
-    if (!tmp)
-        return (1);
-    cnt = 0;
-    ft_strlcpy(tmp, unset, ft_strlen(tmp) + ft_strlen(unset));
-    tmp[ft_strlen(unset)] = '=';
-    tmp[ft_strlen(unset) + 1] = '\0';
-    if (ft_strncmp((char *)g_sh.env->data, tmp, ft_strlen(tmp)) == 0)
-	    return (0);
-    while (g_sh.env && g_sh.env->next)
-    {
-	cnt++;
-        if (ft_strncmp((char *)g_sh.env->next->data, tmp,
-                       ft_strlen(tmp)) == 0)
-        {
-            free(tmp);
-            return (cnt);
-        }
-        g_sh.env = g_sh.env->next;
-    }
-    free(tmp);
-    return (1);
+	if (unset == NULL)
+		return (1);
+	tmp = (char *)malloc(sizeof(char) * (ft_strlen(unset) + 2));
+	if (!tmp)
+		return (1);
+	cnt = 0;
+	copy_and_concat(unset, tmp);
+	if (ft_strncmp((char *)g_sh.env->data, tmp, ft_strlen(tmp)) == 0)
+		return (0);
+	while (g_sh.env && g_sh.env->next)
+	{
+		cnt++;
+		if (ft_strncmp((char *)g_sh.env->next->data, tmp,
+				ft_strlen(tmp)) == 0)
+		{
+			free(tmp);
+			return (cnt);
+		}
+		g_sh.env = g_sh.env->next;
+	}
+	free(tmp);
+	return (1);
 }
 
 /* */
 static void	delete_and_join(t_list *head, int cnt)
 {
-    t_list	*aux;
+	t_list	*aux;
 
-    aux = NULL;
-    if (cnt == 0)
-    {
-   	aux = g_sh.env->next;
-       	ft_lstdelone(g_sh.env, free);
-	g_sh.env = aux;
-	return ;	
-    }
-    if (g_sh.env->next && g_sh.env->next->next)
-        aux = g_sh.env->next->next;
-    ft_lstdelone(g_sh.env->next, free);
-    g_sh.env->next = aux;
-    g_sh.env = head;
+	aux = NULL;
+	if (cnt == 0)
+	{
+		aux = g_sh.env->next;
+		ft_lstdelone(g_sh.env, free);
+		g_sh.env = aux;
+		return ;
+	}
+	if (g_sh.env->next && g_sh.env->next->next)
+		aux = g_sh.env->next->next;
+	ft_lstdelone(g_sh.env->next, free);
+	g_sh.env->next = aux;
+	g_sh.env = head;
 }
 
 /* */
 int	ft_unset(char **unset)
 {
-    t_list	*head;
-    int		i;
-    int 	cnt;
+	t_list	*head;
+	int		i;
+	int		cnt;
 
-    head = g_sh.env;
-    i = 0;
-    if (unset[1])
-    {
-        while (unset[++i])
-        {
-            cnt = find_pos(unset[i]);
-            if (!g_sh.env)
-                g_sh.env = head;
-            else
-                delete_and_join(head, cnt);
-        }
-    }
-    else
-    {
-        write(STDERR_FILENO, "unset: not enough arguments\n", 28);
-        return (1);
-    }
-    g_sh.env = head;
-    return (0);
+	head = g_sh.env;
+	i = 0;
+	if (unset[1])
+	{
+		while (unset[++i])
+		{
+			cnt = find_pos(unset[i]);
+			if (!g_sh.env)
+				g_sh.env = head;
+			else
+				delete_and_join(head, cnt);
+		}
+	}
+	else
+	{
+		write(STDERR_FILENO, "unset: not enough arguments\n", 28);
+		return (1);
+	}
+	g_sh.env = head;
+	return (0);
 }
