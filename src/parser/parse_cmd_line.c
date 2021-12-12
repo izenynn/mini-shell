405d3fb6b204c5a9948f24781a284e7d72894c73
izenynn <me@izenynn.com>
2021-12-12 02:24:54 +0100

    remove comments

--- src/parser/parse_cmd_line.c
+++ src/parser/parse_cmd_line.c
@@ -6,7 +6,7 @@
 /*   By: dpoveda- <me@izenynn.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 11:27:32 by dpoveda-          #+#    #+#             */
-/*   Updated: 2021/12/11 22:12:51 by dpoveda-         ###   ########.fr       */
+/*   Updated: 2021/12/12 00:12:08 by dpoveda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,8 +46,6 @@ t_ast	*cmd_line_1(void)
 	t_ast	*res;
 
 	// <and or>
-	//and_or_nd = NULL;
-	//*g_sh.cmdline_ast = and_or_nd;
 	*g_sh.cmdline_ast = NULL;
 	and_or();
 	and_or_nd = *g_sh.cmdline_ast;
@@ -56,9 +54,7 @@ t_ast	*cmd_line_1(void)
 	// ';'
 	if (!is_term(CHAR_SC, NULL))
 	{
-		//ast_del(and_or_nd);
 		ast_del(and_or_nd);
-		//free(g_sh.cmdline_ast);
 		return (NULL);
 	}
 	while (g_sh.tok != NULL && g_sh.tok->type == CHAR_SC)
@@ -67,15 +63,12 @@ t_ast	*cmd_line_1(void)
 	cmd_line_nd = cmd_line();
 	if (cmd_line_nd == NULL)
 	{
-		//ast_del(and_or_nd);
 		ast_del(and_or_nd);
-		//free(g_sh.cmdline_ast);
 		return (NULL);
 	}
 	// create node
 	res = (t_ast *)malloc(sizeof(t_ast));
 	ast_settype(res, AST_SEQ);
-	//ast_attach(res, and_or_nd, cmd_line_nd);
 	ast_attach(res, and_or_nd, cmd_line_nd);
 	return (res);
 }
@@ -88,8 +81,6 @@ t_ast	*cmd_line_2(void)
 	t_ast	*res;
 
 	// <and or>
-	//and_or_nd = NULL;
-	//*g_sh.cmdline_ast = and_or_nd;
 	*g_sh.cmdline_ast = NULL;
 	and_or();
 	and_or_nd = *g_sh.cmdline_ast;
@@ -98,9 +89,7 @@ t_ast	*cmd_line_2(void)
 	// ';'
 	if (!is_term(CHAR_SC, NULL))
 	{
-		//ast_del(and_or_nd);
 		ast_del(and_or_nd);
-		//free(g_sh.cmdline_ast);
 		return (NULL);
 	}
 	while (g_sh.tok != NULL && g_sh.tok->type == CHAR_SC)
@@ -109,15 +98,12 @@ t_ast	*cmd_line_2(void)
 	cmd_line_nd = cmd_line();
 	if (cmd_line_nd == NULL)
 	{
-		//ast_del(and_or_nd);
 		ast_del(and_or_nd);
-		//free(g_sh.cmdline_ast);
 		return (NULL);
 	}
 	// create node
 	res = (t_ast *)malloc(sizeof(t_ast));
 	ast_settype(res, AST_SEQ);
-	//ast_attach(res, and_or_nd, cmd_line_nd);
 	ast_attach(res, and_or_nd, cmd_line_nd);
 	return (res);
 }
@@ -132,9 +118,6 @@ t_ast	*cmd_line_3(void)
 	and_or();
 	and_or_nd = *g_sh.cmdline_ast;
 	if (and_or_nd == NULL)
-	{
-		//free(g_sh.cmdline_ast);
 		return (NULL);
-	}
 	return (and_or_nd);
 }
