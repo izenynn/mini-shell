0de9c320c0cde5e5ec9ec9e5639f0222302402a8
izenynn <me@izenynn.com>
2021-12-14 01:37:32 +0100

    fix more norme

--- src/parser/parse_cmd_line.c
+++ src/parser/parse_cmd_line.c
@@ -6,7 +6,7 @@
 /*   By: dpoveda- <me@izenynn.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 11:27:32 by dpoveda-          #+#    #+#             */
-/*   Updated: 2021/12/14 00:33:35 by dpoveda-         ###   ########.fr       */
+/*   Updated: 2021/12/14 01:37:20 by dpoveda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ t_ast	*cmd_line(void)
 	t_ast	*new_node;
 
 	save = g_sh.tok;
-
 	g_sh.tok = save;
 	new_node = cmd_line_1();
 	if (new_node != NULL)
